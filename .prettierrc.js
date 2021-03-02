d9a1ddb0f2588f38b8ad6ea15fe9b50c40c2d8dd
Nicolas Guyon <nicolas.guyon@telecomnancy.net>
2021-03-02 17:33:50 +0100

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+    arrowParens: "always",
+    trailingComma: "all",
+    jsxSingleQuote: true,
+}
+
